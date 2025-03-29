49e9f0a57915d495f573e4d4e59580f02e22127a
shihakura <L1NADIK@yandex.ru>
2025-03-29 13:23:32 +0300

    Добавьте файлы проекта.

---
+++ WpfApps/ViewModels/SampleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using WpfCustomControlLibrary.Models;
+
+public class SampleViewModel : BindableBase
+{
+    private SampleModel _model;
+
+    public string Name
+    {
+        get => _model.Name;
+        //set => SetProperty(ref _model.Name, value);
+    }
+
+    public SampleViewModel(SampleModel model)
+    {
+        _model = model;
+    }
+}
