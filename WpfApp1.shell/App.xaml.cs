b404182ee361426dfe6a8e2d6a30e43e96db571b
shihakura <L1NADIK@yandex.ru>
2025-03-30 14:43:19 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1.shell/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfApp1.shell.View;
+
+namespace WpfApp1.shell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell() => Container.Resolve<View.Shell>();
+        
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+
+}
