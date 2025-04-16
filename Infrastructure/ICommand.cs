3f47de0a4cbe9285a73315df8cacf6aae6eb6315
EgorD <ggogogogo521@gmail.com>
2025-04-16 18:23:14 +0300

    Добавил классы ICommand и INotifyPropertyChanged закинул примеры использования джля остальных

---
+++ Infrastructure/ICommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+public class DelegateCommand : ICommand
+{
+    private readonly Action<object> _executeMethod;
+    private readonly Predicate<object> _canExecuteMethod;
+
+    public DelegateCommand(Action<object> executeMethod)
+        : this(executeMethod, null)
+    {
+    }
+
+    public DelegateCommand(Action<object> executeMethod, Predicate<object> canExecuteMethod)
+    {
+        if (executeMethod == null)
+            throw new ArgumentNullException(nameof(executeMethod));
+
+        _executeMethod = executeMethod;
+        _canExecuteMethod = canExecuteMethod;
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecuteMethod == null || _canExecuteMethod(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _executeMethod(parameter);
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Infrastructure/INotifyPropertyChanged.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+public abstract class BaseNotifier : INotifyPropertyChanged
+{
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    
+    protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+    {
+        if (Equals(field, value))
+            return;
+
+        field = value;
+        NotifyPropertyChanged(propertyName);
+    }
+
+    
+    protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    
+    protected void NotifyPropertyChanged(Expression<Func<object>> propertyExpression)
+    {
+        var propertyName = GetPropertyName(propertyExpression);
+        NotifyPropertyChanged(propertyName);
+    }
+
+    
+    private string GetPropertyName(Expression<Func<object>> propertyExpression)
+    {
+        var lambda = (MemberExpression)propertyExpression.Body;
+        return lambda.Member.Name;
+    }
+}
