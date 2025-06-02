0fe2bd4f7323083b34a46d8a5677de8780f24de4
shihakura <L1NADIK@yandex.ru>
2025-06-02 14:07:57 +0300

    еще ссылочек

--- WpfApp1.shell/ViewModel/MainMenuPageViewModel.cs
+++ WpfApp1.shell/ViewModel/MainMenuPageViewModel.cs
@@ -1,9 +1,63 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
 
 namespace WpfApp1.shell.ViewModel
 {
     public class MainMenuPageViewModel : BindableBase
     {
-        // Здесь можно добавить свойства и команды для главного меню.
+        public DelegateCommand OpenMapCommand { get; private set; }
+        public DelegateCommand OpenWebsiteCommand { get; private set; }
+
+        public MainMenuPageViewModel()
+        {
+            OpenMapCommand = new DelegateCommand(OpenMap);
+            OpenWebsiteCommand = new DelegateCommand(OpenWebsite);
+        }
+
+        private void OpenMap()
+        {
+            // Формируем URL для Яндекс.Карт (можно заменить на Google Maps или другой сервис)
+            string address = "Вологда, улица Кирова, 35";
+            string url = $"https://yandex.ru/maps/?text={System.Web.HttpUtility.UrlEncode(address)}";
+
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось открыть карту. Проверьте подключение к интернету.",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void OpenWebsite()
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://lic32-vologda-r19.gosweb.gosuslugi.ru/",
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось открыть сайт. Проверьте подключение к интернету.",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
     }
 }
